525ec5f3801358f2e5fb3675d1f62e3a215fa225
Ofir Gilad <85561260+OfirGilad369@users.noreply.github.com>
2024-05-21 11:16:21 +0300

    fixes

--- video_predict.py
+++ video_predict.py
@@ -7,8 +7,11 @@ from ultralytics.utils.plotting import Annotator, colors
 
 from collections import defaultdict
 
+import os
+import pandas as pd
 
-def predict(video_path: str):
+
+def predict(video_path: str, export_filepath: str):
     track_history = defaultdict(lambda: [])
     model = YOLO("yolov8n.pt")
     names = model.model.names
@@ -19,14 +22,18 @@ def predict(video_path: str):
     w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
 
     result = cv2.VideoWriter(
-        "object_tracking.avi",
+        f"{video_path}_annotations.avi",
         cv2.VideoWriter_fourcc(*'mp4v'),
         fps,
         (w, h)
     )
 
+    tracking_results = list()
+    frame_number = -1
     while cap.isOpened():
         success, frame = cap.read()
+        frame_number += 1
+
         if success:
             results = model.track(frame, persist=True, verbose=False)
             boxes = results[0].boxes.xyxy.cpu()
@@ -57,11 +64,30 @@ def predict(video_path: str):
                     cv2.circle(frame, (track[-1]), 7, colors(int(cls), True), -1)
                     cv2.polylines(frame, [points], isClosed=False, color=colors(int(cls), True), thickness=2)
 
+                    # Aggregate tracking results
+                    tracking_results.append({
+                        "frame_number": frame_number,
+                        "track_id": int(track_id),
+                        "label": names[int(cls)],
+                        "x1": int(box[0]),
+                        "y1": int(box[1]),
+                        "x2": int(box[2]),
+                        "y2": int(box[3]),
+                        "width": int(box[2] - box[0]),
+                        "height": int(box[3] - box[1]),
+                        "confidence": conf
+                    })
+
             result.write(frame)
             if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
         else:
             break
+
+    # export the results
+    tracking_results.sort(key=lambda x: x["frame_number"])
+    df = pd.DataFrame(tracking_results)
+    df.to_csv(export_filepath, index=False)
 
     result.release()
     cap.release()
@@ -69,13 +95,14 @@ def predict(video_path: str):
 
 
 # TODO: Add convert of annotations to csv file:
-# Frame Number, Track ID, Label, X, Y, Width, Height, Confidence
+# Frame Number, Track ID, Label, X1, Y1, X2, Y2, Confidence
 
 # TODO: Find model for the eye tracker (maybe 1D Conv)
 def main():
-    video_path = "./pexels-thirdman-5538262 (1080p).mp4"
+    video_path = os.path.join(os.getcwd(), "pexels-thirdman-5538262 (1080p).mp4")
+    export_filepath = f"{video_path}_annotations.csv"
 
-    predict(video_path=video_path)
+    predict(video_path=video_path, export_filepath=export_filepath)
 
 
 if __name__ == '__main__':
