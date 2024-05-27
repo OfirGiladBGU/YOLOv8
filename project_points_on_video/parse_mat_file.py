2392e9bcd09c1e548f36c708bcfbb38b648e0d28
Ofir Gilad <ofir.gilad9631@gmail.com>
2024-05-28 01:22:51 +0300

    converter in progress

---
+++ project_points_on_video/parse_mat_file.py
@@ -0,0 +1,11 @@
+import scipy.io
+import pandas as pd
+
+
+if __name__ == '__main__':
+    mat_filepath = r"C:\Users\Ofir Gilad\PycharmProjects\YOLOv8\project_points_on_video\input\control_dinstein_girls.mat"
+    mat = scipy.io.loadmat(mat_filepath)
+
+    df = pd.DataFrame(mat)
+    csv_filepath = r"C:\Users\Ofir Gilad\PycharmProjects\YOLOv8\project_points_on_video\output\control_dinstein_girls.csv"
+    df.to_csv('your_data.csv')  # your_data.csv final data in csv file
