95625ded1c9ab8d6918821693e4acc3d10ffd6d2
Ofir Gilad <85561260+OfirGilad369@users.noreply.github.com>
2024-06-11 20:22:24 +0300

    small fix

--- project_points_on_video_V2/video_resize.py
+++ project_points_on_video_V2/video_resize.py
@@ -2,7 +2,7 @@ import moviepy.editor as mp
 
 
 if __name__ == '__main__':
-    video_path = r"C:\Users\Ofir Gilad\PycharmProjects\YOLOv8\dataloop_annotations\girls.mp4"
+    video_path = "../dataloop_annotations/girls.mp4"
     clip = mp.VideoFileClip(video_path)
-    clip_resized = clip.resize(width=1024, height=1280)
+    clip_resized = clip.resize(width=1024)
     clip_resized.write_videofile("output_girls.mp4")
