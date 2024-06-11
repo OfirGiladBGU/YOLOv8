f24f0957f5e92d47cd8e34954f79041c65977d02
Ofir Gilad <85561260+OfirGilad369@users.noreply.github.com>
2024-06-11 18:40:06 +0300

    small save

--- project_points_on_video/parse_mat_file.py
+++ project_points_on_video/parse_mat_file.py
@@ -1,11 +1,26 @@
+# OPTION 1
+
+# import scipy.io
+# import pandas as pd
+#
+#
+# if __name__ == '__main__':
+#     mat_filepath = r"C:\Users\Ofir Gilad\PycharmProjects\YOLOv8\project_points_on_video\input\control_dinstein_girls.mat"
+#     mat = scipy.io.loadmat(mat_filepath)
+#
+#     df = pd.DataFrame(mat)
+#     csv_filepath = r"C:\Users\Ofir Gilad\PycharmProjects\YOLOv8\project_points_on_video\output\control_dinstein_girls.csv"
+#     df.to_csv('your_data.csv')  # your_data.csv final data in csv file
+
+# OPTION 2
+
 import scipy.io
 import pandas as pd
 
 
 if __name__ == '__main__':
-    mat_filepath = r"C:\Users\Ofir Gilad\PycharmProjects\YOLOv8\project_points_on_video\input\control_dinstein_girls.mat"
-    mat = scipy.io.loadmat(mat_filepath)
+	mat_filepath = r"C:\Users\Ofir Gilad\PycharmProjects\YOLOv8\project_points_on_video\control_dinstein_girls.mat"
+	mat = scipy.io.loadmat(mat_filepath)
 
-    df = pd.DataFrame(mat)
-    csv_filepath = r"C:\Users\Ofir Gilad\PycharmProjects\YOLOv8\project_points_on_video\output\control_dinstein_girls.csv"
-    df.to_csv('your_data.csv')  # your_data.csv final data in csv file
+	with open("./output/out.txt", "w") as f:
+		f.write(str(mat))
